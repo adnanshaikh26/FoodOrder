258146f72b185efd7e5ac3e03b0d10a570dd7ba3
adnanshaikh26 <adnan.shk.26@gmail.com>
2024-02-21 17:27:40 +0530

    Deploying to GitHub Pages

---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  FaDribbbleSquare,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaTwitterSquare,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="max-w-[1520px] m-auto px-4 py-2 bg-[#24262b]">
+      <div className="py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
+        <div>
+          <h1 className="w-full text-3xl font-bold text-orange-500">YumEats</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            tenetur accusamus quis officiis in aperiam rerum, et, fugit pariatur
+            vel provident est cum quidem, eligendi consequuntur labore obcaecati
+            inventore illo.
+          </p>
+          <div className="flex justify-center md:w-[75%] my-6">
+            <FaFacebookSquare size={30} />
+            <FaInstagram size={30} />
+            <FaTwitterSquare size={30} />
+            <FaGithubSquare size={30} />
+            <FaDribbbleSquare size={30} />
+          </div>
+        </div>
+        <div className="lg:col-span-2 flex justify-between mt-6">
+          <div>
+            <h6 className="font-medium text-[#9b9b9b9]">Location</h6>
+            <ul>
+              <li className="py-2 text-sm">Pune</li>
+              <li className="py-2 text-sm">Mumbai</li>
+              <li className="py-2 text-sm">Agra </li>
+              <li className="py-2 text-sm">Mahabaleshwar</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
